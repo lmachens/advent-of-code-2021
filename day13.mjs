4bdf5e5603452b31d6867aff7c7a4614b0c46991
lmachens <leon@machens.koeln>
2021-12-14 10:18:57 +0100

    🔥 Remove comment

--- day13.mjs
+++ day13.mjs
@@ -1,27 +1,6 @@
 import { getTask } from "./lib.js";
 
 const task = await getTask(13);
-// const task = `6,10
-// 0,14
-// 9,10
-// 0,3
-// 10,4
-// 4,11
-// 6,0
-// 6,12
-// 4,1
-// 0,13
-// 10,12
-// 3,4
-// 3,0
-// 8,4
-// 1,10
-// 2,14
-// 8,10
-// 9,0
-
-// fold along y=7
-// fold along x=5`;
 
 const input = task.split("\n");
 const separator = input.indexOf("");
